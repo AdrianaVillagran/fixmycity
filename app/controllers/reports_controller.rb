045421b5bfa8dce127aed8451f5046c9f9bd9c55
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-29 02:10:10 -0700

    added create and show actions and views

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,25 @@
 class ReportsController < ApplicationController
   def index
     @reports = Report.all
+    @report = Report.new
   end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      redirect_to report_path(@report)
+    end
+  end
+
+  def show
+    @report = Report.find_by_id(params[:id])
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :description, :category, :major_road,
+                                   :cross_street1, :cross_street2)
+  end
+
 end

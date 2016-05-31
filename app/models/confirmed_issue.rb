9623851a0726421bf34cbf0b2c8779bc15d63044
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-31 16:02:54 -0700

    added category to report and confirmed issue models

--- app/models/confirmed_issue.rb
+++ app/models/confirmed_issue.rb
@@ -2,7 +2,7 @@ class ConfirmedIssue < ActiveRecord::Base
   has_and_belongs_to_many :reports
 
   enum category: [:pothole, :sidewalk, :streetlight, :traffic_signal, :signage,
-                  :sinkhole, :pavement, :powerline, :vandalism, :animal_control,
+                  :road_obstruction, :pavement, :powerline, :vandalism, :animal_control,
                   :other]
   enum status: [:pending, :in_progress, :closed, :hidden]
   enum danger_level: [:mild, :moderate, :severe]
--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,7 @@ class Report < ActiveRecord::Base
   has_and_belongs_to_many :confirmed_issues
 
   enum category: [:pothole, :sidewalk, :streetlight, :traffic_signal, :signage,
-                  :sinkhole, :pavement, :powerline, :vandalism, :animal_control,
+                  :road_obstruction, :pavement, :powerline, :vandalism, :animal_control,
                   :other]
   enum status: [:active, :confirmed, :flagged, :deleted, :hidden]
   enum danger_level: [:mild, :moderate, :severe]

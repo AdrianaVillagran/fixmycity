89732df714f61855904b975946d8357532a02489
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-31 10:40:35 -0700

    added before action to set confirmed issue

--- app/controllers/confirmed_issues_controller.rb
+++ app/controllers/confirmed_issues_controller.rb
@@ -1,5 +1,6 @@
 class ConfirmedIssuesController < ApplicationController
   before_action :set_report
+  before_action :set_confirmed_issue
 
   def index
     @confirmed_issues = ConfirmedIssue.all
@@ -15,6 +16,10 @@ class ConfirmedIssuesController < ApplicationController
   end
 
   private
+
+  def set_confirmed_issue
+    @confirmed_issue = ConfirmedIssue.find_by_id(params[:id])
+  end
 
   def set_report
     # set report for report and issue button in navbar

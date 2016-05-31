eb37bf66e454d1e914260046fd0deefd063a7d7f
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-31 10:17:58 -0700

    set confirmed_issues for index action and added before action to define report for report an issue button in navbar

--- app/controllers/confirmed_issues_controller.rb
+++ app/controllers/confirmed_issues_controller.rb
@@ -1,5 +1,8 @@
 class ConfirmedIssuesController < ApplicationController
+  before_action :set_report
+
   def index
+    @confirmed_issues = ConfirmedIssue.all
   end
 
   def show
@@ -9,5 +12,12 @@ class ConfirmedIssuesController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def set_report
+    # set report for report and issue button in navbar
+    @report = Report.new
   end
 end

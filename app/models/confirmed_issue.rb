9b94d5dd6e7f1b519812f42dd8a99ba5e543d4ed
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-31 15:04:38 -0700

    defined associations in confirmed_issue and report models

--- app/models/confirmed_issue.rb
+++ app/models/confirmed_issue.rb
@@ -1,4 +1,6 @@
 class ConfirmedIssue < ActiveRecord::Base
+  has_and_belongs_to_many :reports
+
   enum category: [:pothole, :sidewalk, :streetlight, :traffic_signal, :signage,
                   :sinkhole, :pavement, :powerline, :vandalism, :other]
   enum status: [:pending, :in_progress, :closed, :hidden]
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,5 @@
 class Report < ActiveRecord::Base
+  has_and_belongs_to_many :confirmed_issues
+
   enum status: [:active, :confirmed, :flagged, :deleted, :hidden]
 end

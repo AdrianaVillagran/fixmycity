aa679ef8cda60a6b465aff19853cd7ead5b769ce
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-31 15:23:56 -0700

    defined enum options for category and danger level

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,9 @@
 class Report < ActiveRecord::Base
   has_and_belongs_to_many :confirmed_issues
 
+  enum category: [:pothole, :sidewalk, :streetlight, :traffic_signal, :signage,
+                  :sinkhole, :pavement, :powerline, :vandalism, :animal_control,
+                  :other]
   enum status: [:active, :confirmed, :flagged, :deleted, :hidden]
+  enum danger_level: [:mild, :moderate, :severe]
 end

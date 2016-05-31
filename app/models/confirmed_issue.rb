54498280e2fde980f4ca0e134b84f40313839c79
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-31 15:22:59 -0700

    added animal control category to report

--- app/models/confirmed_issue.rb
+++ app/models/confirmed_issue.rb
@@ -2,7 +2,8 @@ class ConfirmedIssue < ActiveRecord::Base
   has_and_belongs_to_many :reports
 
   enum category: [:pothole, :sidewalk, :streetlight, :traffic_signal, :signage,
-                  :sinkhole, :pavement, :powerline, :vandalism, :other]
+                  :sinkhole, :pavement, :powerline, :vandalism, :animal_control,
+                  :other]
   enum status: [:pending, :in_progress, :closed, :hidden]
   enum danger_level: [:mild, :moderate, :severe]
 end

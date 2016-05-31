a768db890aaac578fb465e35b4c7375606ae5405
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-31 15:03:44 -0700

    generated confirmed_issues_reports join table migration file

--- db/migrate/20160531210208_create_join_table_confirmed_issues_reports.rb
+++ db/migrate/20160531210208_create_join_table_confirmed_issues_reports.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableConfirmedIssuesReports < ActiveRecord::Migration
   def change
     create_join_table :confirmed_issues, :reports do |t|
-      t.index [:confirmed_issue_id, :report_id]
-      t.index [:report_id, :confirmed_issue_id]
+      t.index([:confirmed_issue_id, :report_id], name: "idx_confirmed_issue_report")
+      t.index([:report_id, :confirmed_issue_id], name: "idx_report_confirmed_issue")
     end
   end
 end

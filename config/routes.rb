cbdda1180faab4b542aaf7c11dea2ce266c33691
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-31 09:21:43 -0700

    created routes for confirmed issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: "reports#index"
 
   get 'site/index'
-  resources :reports
+
   post 'reports/:id/confirm', to: "reports#confirm", as: 'confirm_report'
+  resources :reports
+
+  resources :confirmed_issues
+  
 end

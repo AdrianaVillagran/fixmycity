24016767c718a28951a0c31bacf9edab8a4ae2ea
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-31 15:04:07 -0700

    migrated join table to update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531183324) do
+ActiveRecord::Schema.define(version: 20160531210208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20160531183324) do
     t.datetime "updated_at",    null: false
   end
 
+  create_table "confirmed_issues_reports", id: false, force: :cascade do |t|
+    t.integer "confirmed_issue_id", null: false
+    t.integer "report_id",          null: false
+  end
+
+  add_index "confirmed_issues_reports", ["confirmed_issue_id", "report_id"], name: "idx_confirmed_issue_report", using: :btree
+  add_index "confirmed_issues_reports", ["report_id", "confirmed_issue_id"], name: "idx_report_confirmed_issue", using: :btree
+
   create_table "reports", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
@@ -42,13 +50,9 @@ ActiveRecord::Schema.define(version: 20160531183324) do
     t.float    "longitude"
     t.integer  "upvotes"
     t.integer  "downvotes"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.integer  "status",             default: 0
-    t.integer  "confirmed_issue_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "status",        default: 0
   end
 
-  add_index "reports", ["confirmed_issue_id"], name: "index_reports_on_confirmed_issue_id", using: :btree
-
-  add_foreign_key "reports", "confirmed_issues"
 end
